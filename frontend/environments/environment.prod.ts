caaf4d32bbf7e62b66b483af0330f92019ff4918
Analice Silva <analiceds.nascimento@gmail.com>
2025-09-09 16:25:45 -0300

    [frontend][chore] Atualiza arquivos de configuração de ambiente e bootstrap

--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://plataforma-de-mentoria-backend.onrender.com",
-  baseApiUrl: 'https://plataforma-de-mentoria-backend.onrender.com'
+  apiUrl: "https://plataforma-de-mentoria-backend.onrender.com/api",
+  baseApiUrl: 'https://plataforma-de-mentoria-backend.onrender.com/api'
 };
